3ab30356564fc0cb356a1990bc4f3ba2593e5296
yachna gupta <yachnagupta04@gmail.com>
2026-01-04 12:55:25 +0530

    epdated ecobud

--- README.md
+++
@@ -1,1 +0,0 @@
-# ecobud-backend-
--- src/routes/TrainingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { markVideoWatched } = require("../controllers/trainingController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.post("/watch-video", protect, markVideoWatched);
-
-module.exports = router;
